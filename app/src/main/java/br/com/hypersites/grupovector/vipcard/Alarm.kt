4d4ac79d3ed3228ecf60eb028af7b4c866b9ec56
alexandre.andrade <alexandre.andrade@comerc.com.br>
2018-03-21 19:27:19 -0300

    Ajustando Fluxo de navegação

--- app/src/main/java/br/com/hypersites/grupovector/vipcard/Alarm.kt
+++ app/src/main/java/br/com/hypersites/grupovector/vipcard/Alarm.kt
@@ -1,9 +1,12 @@
 package br.com.hypersites.grupovector.vipcard
 
+import android.app.Service
+import android.content.Context
+
 /**
  * Created by alexandre.andrade on 21/03/18.
  */
-class Alarm :  {
+class Alarm {
     fun Alarm(){
 
     }
