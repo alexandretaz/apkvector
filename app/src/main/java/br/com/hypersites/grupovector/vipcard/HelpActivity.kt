88ddf89c4b711a62f9093c8762f7cf948a01d8ee
Alexandre dos Santos Andrade <alexandretaz@gmail.com>
2018-04-03 06:56:00 -0300

    Fixing Main Activity and Alarm service to send message and points. Stopping alarm and restarting clicking on Home

--- app/src/main/java/br/com/hypersites/grupovector/vipcard/HelpActivity.kt
+++ app/src/main/java/br/com/hypersites/grupovector/vipcard/HelpActivity.kt
@@ -1,55 +1,60 @@
 package br.com.hypersites.grupovector.vipcard
 
 import android.Manifest
+import android.annotation.SuppressLint
 import android.content.pm.PackageManager
 import android.os.Build
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.v4.app.ActivityCompat
 import android.support.v4.content.ContextCompat
-
-import com.google.android.gms.maps.CameraUpdateFactory
-import com.google.android.gms.maps.GoogleMap
-import com.google.android.gms.maps.OnMapReadyCallback
-import com.google.android.gms.maps.SupportMapFragment
-import com.google.android.gms.maps.model.LatLng
-import com.google.android.gms.maps.model.MarkerOptions
+import android.content.Intent
+import android.net.Uri
+import android.support.v7.widget.RecyclerView
+import android.widget.Button
 import kotlinx.android.synthetic.main.activity_help.*
 
 class HelpActivity : AppCompatActivity(){
 
     //private lateinit var mMap: GoogleMap
 
+    @SuppressLint("MissingPermission")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_help)
-        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+       val btn_call = findViewById<RecyclerView>(R.id.callButton) as Button
+        btn_call.setOnClickListener {
+            runtime_permissions()
+            val intent = Intent(Intent.ACTION_CALL, Uri.parse("tel:" + "+551136489340"))
+            startActivity(intent)
+        }
     }
 
     private fun runtime_permissions():Boolean {
+        var location:Int = 0
+        var phone:Int = 0
+        var total:Int = 0
         if(Build.VERSION.SDK_INT>=23 &&
                 ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
                 != PackageManager.PERMISSION_GRANTED && ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION)
                 != PackageManager.PERMISSION_GRANTED) {
             ActivityCompat.requestPermissions(this,arrayOf(Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION)
                     , 100)
-            return true;
+             location = 1
+        }
+
+        if(Build.VERSION.SDK_INT>=23 &&
+                ContextCompat.checkSelfPermission(this, Manifest.permission.CALL_PHONE)
+                != PackageManager.PERMISSION_GRANTED ) {
+            ActivityCompat.requestPermissions(this,arrayOf(Manifest.permission.CALL_PHONE)
+                    , 100)
+            phone = 1
+        }
+        total = (location+phone)
+        if(total==2) {
+            return true
         }
         return false
-    }
-
-    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
-        if(requestCode == 100)
-        {
-            if(grantResults[0]==PackageManager.PERMISSION_GRANTED && grantResults[1] == PackageManager.PERMISSION_GRANTED)
-            {
-
-            }
-            else{
-                requestPermissions(arrayOf(Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION),100)
-            }
-        }
     }
 
 
